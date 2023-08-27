d711c3618b97bbb30a4b4198cb69b4c7534e6671
Rizwan0994 <f190994@nu.edu.pk>
2023-08-27 17:25:59 +0500

    deploy

--- app.py
+++ app.py
@@ -2,33 +2,31 @@ from flask import Flask, request, jsonify
 from PIL import Image
 import torch
 from transformers import BlipProcessor, BlipForConditionalGeneration
-from flask_cors import CORS
-import os
+from flask_cors import CORS  # Import CORS
 
 app = Flask(__name__)
-CORS(app)
-
+CORS(app)  # Add CORS so that frontend can access backend API
 processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
 model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
 
 @app.route('/predict_caption', methods=['POST'])
 def predict_caption():
-    img_data = request.files.get('image_data')
-    text = "a photography of"
-
+    img_data = request.files.get('image_data')  # Get image data from POST request
+    text = "a photography of"  # You can customize the text
+    
+    # Convert FileStorage to PIL.Image.Image
     image = Image.open(img_data).convert('RGB')
-
+    
     inputs = processor(image, text, return_tensors="pt")
     output = model.generate(**inputs)
     caption = processor.decode(output[0], skip_special_tokens=True)
+    print(caption)
     return jsonify({'caption': caption})
 
 @app.route('/test', methods=['GET'])
 def test_endpoint():
     return "Testing App"
 
-# This block ensures the app runs only when executed directly, not when imported as a module
 if __name__ == '__main__':
-    host = os.environ.get('HOST', '0.0.0.0')
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host=host, port=port)
+    app.run()
+
